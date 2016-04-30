b7068a58e905f540352861a6fd5bc9addb8d929d
sepehrvakili <sdvakili@gmail.com>
2016-04-29 18:29:16 -0700

    Add button to add cirlces

--- client/watchout.js
+++ client/watchout.js
@@ -20,22 +20,40 @@ var allCircles = svg
  .attr('cx', random)
  .attr('cy', random)
  .attr('r', 7.5)
- .classed('circles', true);
+ .classed('circle', true);
 
-
-var movefunction = function(){
+var counter = 0;
+var moveFunction = function() {
   // make a var for every circle
-  var circles = svg.selectAll('.circle');
+  svg
+ .selectAll('.circle')
+ .transition()
+ .duration(1000)
+ .attr('cx', random)
+ .attr('cy', random);
 
-  for (var i = 0; i < circles.length; i++) {
-     circles[i].attr('cx', random);
-     circles[i].attr('cy', random);
-  }
+ if (counter > 0) {
+  var newCircles = circles.slice(circles.length - 1 - counter);
+  svg
+ .selectAll('.circle')
+ .data(newCircles)
+ .enter()
+ .append('circle')
+ .attr('cx', random)
+ .attr('cy', random)
+ .attr('r', 7.5)
+ .classed('circle', true);
+ }
   //then change attr of every circles cx and cy to random (invoke random function)
   // use a transition
-}
+};
 
 
-setTimeOut(moveFunction, 500);
+setInterval(moveFunction, 1000);
+
+var addCircle = function() {
+  counter++;
+  circles.push(circles[circles.length - 1] + 1);
+};
 
 
